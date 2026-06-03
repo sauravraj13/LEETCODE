aad5b80681d8c3bfeb1fab763587dbe87f9577cc
saurav-raj-anand <sauravraj123498@gmail.com>
2026-06-03 04:32:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (25.73%)

--- 0908-middle-of-the-linked-list/solution.cpp
+++ 0908-middle-of-the-linked-list/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) return head;
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
+       if (head ==nullptr|| head -> next == nullptr){
+        return head;
+       }
+       ListNode* slow = head;
+       ListNode* fast = head;
+       while(fast != nullptr&& fast->next !=nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+       }
+       return slow;
+}
 };
