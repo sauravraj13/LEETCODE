40ab123cad31d25fccf21e6452774813f11c923c
saurav-raj-anand <sauravraj123498@gmail.com>
2026-06-23 17:58:19 +0000

    Sync LeetCode submission Runtime - 11 ms (65.66%), Memory - 70.8 MB (44.62%)

---
+++ 0456-132-pattern/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        stack<int> st;
+        int second = INT_MIN;
+
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (nums[i] < second)
+                return true;
+
+            while (!st.empty() && nums[i] > st.top()) {
+                second = st.top();
+                st.pop();
+            }
+
+            st.push(nums[i]);
+        }
+
+        return false;
+    }
+};
